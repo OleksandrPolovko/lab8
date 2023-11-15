612e195d3bf8c7be80be0135c8d574c25f1c40a0
OleksandrPolovko <O.Polovko_FIT_12_22_B_d@knute.edu.ua>
2023-11-15 13:59:18 +0200

    Task1

---
+++ ConfigurationManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8
+{
+    public class ConfigurationManager
+    {
+        private static ConfigurationManager _instance;
+
+        // Конфігураційні налаштування
+        public string LogLevel { get; set; }
+        public string DatabaseConnectionString { get; set; }
+
+        // Заборона створення екземплярів ззовні
+        private ConfigurationManager() { }
+
+        // Отримання єдиного екземпляру ConfigurationManager
+        public static ConfigurationManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new ConfigurationManager();
+                }
+                return _instance;
+            }
+        }
+
+        // Метод для виведення конфігураційних налаштувань
+        public void DisplayConfiguration()
+        {
+            Console.WriteLine($"LogLevel: {LogLevel}");
+            Console.WriteLine($"DatabaseConnectionString: {DatabaseConnectionString}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Отримання єдиного екземпляру ConfigurationManager
+            ConfigurationManager configManager = ConfigurationManager.Instance;
+
+            // Виведення початкових конфігураційних налаштувань
+            configManager.DisplayConfiguration();
+
+            // Зміна конфігураційних налаштувань через консольний інтерфейс
+            Console.WriteLine("\nEnter new configuration settings:");
+            Console.Write("LogLevel: ");
+            configManager.LogLevel = Console.ReadLine();
+
+            Console.Write("DatabaseConnectionString: ");
+            configManager.DatabaseConnectionString = Console.ReadLine();
+
+            // Виведення змінених конфігураційних налаштувань
+            Console.WriteLine("\nUpdated configuration settings:");
+            configManager.DisplayConfiguration();
+
+            // Перевірка, що зміни відображаються у єдиному екземплярі ConfigurationManager
+            ConfigurationManager anotherConfigManager = ConfigurationManager.Instance;
+            Console.WriteLine("\nConfiguration settings from another instance:");
+            anotherConfigManager.DisplayConfiguration();
+        }
+    }
+}
